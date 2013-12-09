1172b5562a8be3ca6f4506d1cff9f5ee040d7a32
Caroline Pate <carolinp@live.unc.edu>
2013-12-09 03:27:24 -0500

    Fix text errors and infinite scroll in scraping script; add data results

--- TableScraperScript.py
+++ TableScraperScript.py
@@ -1,6 +1,6 @@
-rom bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import urllib2
-site = "http://www.grammy.com/nominees/search?artist=&field_nominee_work_value=$
+site = "http://www.grammy.com/nominees/search?page=5&artist=&field_nominee_work_value=&year=All&genre=31"
 page = urllib2.urlopen(site)
 soup = BeautifulSoup(page)
 
@@ -14,19 +14,19 @@ f = open('output.csv', 'w')
 
 for row in table.findAll("tr"):
     cells = row.findAll("td")
-
-   if len(cells) == 4:
+ 
+    if len(cells) == 4:
         year = cells[0].find(text=True)
         category = cells[1].find(text=True)
         work = cells[2].find(text=True)
-        for cells[3] in row.findAll("p"):
-            link = cells[3].findAll("a")
+	for cells[3] in row.findAll("p"):
+	    link = cells[3].findAll("a")
             if len(link) == 1:
-                performer = link[0].find(text=True)
-            elif len(link) == 0:
-                performer = cells[3].find(text=True)
-        result = year + "," + category + "," + work + "," + performer
-        print result
-        f.write(result)
+	        performer = link[0].find(text=True)
+	    elif len(link) == 0:
+	        performer = cells[3].find(text=True)
+	result = year + ";" + category + ";" + work + ";" + performer
+	f.write(result)
+	#print result
 
 f.close()
