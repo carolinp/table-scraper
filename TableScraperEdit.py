777bc756ddf3c579395e3bff951a097bb51511cd
Caroline Pate <carolinp@live.unc.edu>
2013-12-09 18:29:02 -0500

    Add comments

--- TableScraperEdit.py
+++ TableScraperEdit.py
@@ -1,15 +1,21 @@
+#Imports the libraries we'll be using. Make sure you have these installed
+#first by typing import into python and seeing if it does anything
 from bs4 import BeautifulSoup
 import urllib2
 
+#These are the editable variables in the scraper. The user defines them by
+#typing their answers to the input questions in quotation marks.
 url = input('Paste the url you want to use: ')
 css = input('What kind of tag are you looking for (class or id)?: ')
 tag = input('What are the cells tagged as?: ')
 file_name = input('What would you like to name the data file?: ')
 
+#Defining and opening the site that will be used
 site = url
 page = urllib2.urlopen(site)
 soup = BeautifulSoup(page)
 
+#Empty variables that the data from each cell will go into later
 cell1 = ""
 cell2 = ""
 cell3 = ""
@@ -17,19 +23,28 @@ cell4 = ""
 cell5 = ""
 cell6 = ""
 
+#Defines what we're looking for (a table) and what the table we're looking for
+#is tagged as - either a css class or id that's defined by the user
 table = soup.find("table", {css : tag})
 
+#Opens the file that the data will go into and gives it a filename defined 
+#by the user
 f = open(file_name, 'w')
 
+#Iterates through each row in the table to find each cell
 for row in table.findAll("tr"):
     cells = row.findAll("td")
+
+#Defines the number of cells we're looking for, finds the text in each of 
+#those cells, and makes the empty variables from earlier equal to the text.
+#Right now, the scraper can scrape tables with up to six columns.Then it writes
+#to the file in the csv format, with each cell surrounded by semicolons
     if len(cells) == 2:
         cell1 = cells[0].find(text=True)
         cell2 = cells[1].find(text=True)
         result = cell1 + ";" + cell2 
         f.write(result)
        
-
     if len(cells) == 3:
         cell1 = cells[0].find(text=True)
         cell2 = cells[1].find(text=True)
@@ -64,10 +79,10 @@ for row in table.findAll("tr"):
         cell4 = cells[3].find(text=True)
         cell5 = cells[4].find(text=True)
 	cell6 = cells[5].find(text=True)
-        result = cell1 + ";" + cell2 + ";" + cell3 + ";" + cell4 + ":" + cell5 + ";" + cell6
+	result = cell1 + ";" + cell2 + ";" + cell3 + ";" + cell4 + ":" + cell5 + ";" + cell6
         f.write(result)
         
 
-
+#Closes the file and prints to let the user know that it's done
 f.close()
 print "Done!"
--- TableScraperScript.py
+++ TableScraperScript.py
@@ -1,32 +1,48 @@
+#Imports the libraries we'll be using. Make sure you have these installed
+#first
 from bs4 import BeautifulSoup
 import urllib2
+#Defines the webpage that we'll be scraping from
 site = "http://www.grammy.com/nominees/search?page=5&artist=&field_nominee_work_value=&year=All&genre=31"
 page = urllib2.urlopen(site)
 soup = BeautifulSoup(page)
 
+#Empty variables that the data from each cell will go into later
 year = ""
 category = ""
 performer = ""
 work = ""
+
+#Defines what we're looking for (a table) and what it's tagged as so it can
+#easily be found (in this case, a specific css class)
 table = soup.find("table", {"class" : "views-table cols-4"})
 
+#Opens the file that the data will go into and defines the file format/name
 f = open('output.xml', 'w')
 
+#Iterates through each row in the table to find each cell
 for row in table.findAll("tr"):
     cells = row.findAll("td")
- 
+
+#Defines the number of cells we're looking for, finds the text in each of 
+#those cells, and makes the empty variables from earlier equal to the text
     if len(cells) == 4:
         year = cells[0].find(text=True)
         category = cells[1].find(text=True)
         work = cells[2].find(text=True)
+#This particular cell was a little wonky and had text housed in either a 
+#paragraph or a link, so I went through each of these cells and told it to
+#find the text in either a link or a paragraph tag
 	for cells[3] in row.findAll("p"):
 	    link = cells[3].findAll("a")
             if len(link) == 1:
 	        performer = link[0].find(text=True)
 	    elif len(link) == 0:
 	        performer = cells[3].find(text=True)
+#Writes to the file in the xml format, with each cell surrounded by tags and 
+#prints to let us know that it's finished
 	f.write("<dataTable>" + "<year>" + year + "</year>" + "<category>" + category + "</category>" + "<work>" + work + "</work>" + "<performer>" + performer + "</performer>" + "</dataTable>")
-	#f.write(result)
-	#print result
+	print "Done!"	
 
+#Closes the file
 f.close()
